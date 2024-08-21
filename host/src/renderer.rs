b365300697394150fb831acab0e446aa4eedbcf5
PancakeTAS <pancake@mgnet.work>
2024-08-21 15:54:52 +0200

    feat: improve safety in render pipeline

--- host/src/renderer.rs
+++ host/src/renderer.rs
@@ -1,7 +1,9 @@
-use std::{collections::HashMap, error::Error, path::PathBuf};
+use std::{collections::HashMap, path::PathBuf};
 
+use anyhow::{anyhow, Context};
 use egls::{so::SharedObject, Environment};
 use framebuffer::Framebuffer;
+use log::{debug, trace};
 use shaders::Shader;
 use textures::Texture;
 use vertices::VertexArrayObject;
@@ -23,7 +25,7 @@ pub struct RenderPipeline {
     textures: HashMap<u64, Texture>, // screen textures
     shader_program: HashMap<u64, (Shader, Framebuffer)>, // active shader program
 
-    vertex_array: VertexArrayObject, // vertex array object
+    vertex_array: Option<VertexArrayObject>, // vertex array object
 }
 
 impl RenderPipeline {
@@ -35,9 +37,14 @@ impl RenderPipeline {
     ///
     /// * `display` - The wayland display object
     ///
-    pub fn new(display: ObjectId) -> Result<Self, Box<dyn Error>> {
+    /// # Errors
+    ///
+    /// This function will fail if the egl environment or required opengl objects cannot be created
+    ///
+    pub fn new(display: ObjectId) -> Result<Self, anyhow::Error> {
         // create environment
-        let (env, _libgl) = unsafe { env::create_environment(display)? };
+        let (env, _libgl) = unsafe { env::create_environment(display).map_err(|e| anyhow!(e))? };
+        debug!("created egl & gl environment");
 
         // create vao
         let vertex_array = VertexArrayObject::new(
@@ -52,8 +59,9 @@ impl RenderPipeline {
                 0, 1, 3,
                 1, 2, 3
             ]
-        )?;
+        ).map_err(|e| anyhow!(e))?;
         vertex_array.bind();
+        debug!("created vertex array object");
 
         // setup opengl
         unsafe {
@@ -66,7 +74,7 @@ impl RenderPipeline {
             _libgl,
             textures: HashMap::new(),
             shader_program: HashMap::new(),
-            vertex_array
+            vertex_array: Some(vertex_array)
         })
     }
 
@@ -78,17 +86,22 @@ impl RenderPipeline {
     /// * `tid` - The texture id
     /// * `bo` - The buffer object
     ///
-    pub fn set_texture(&mut self, tid: u64, bo: &gbm::BufferObject<()>) -> Result<(), Box<dyn Error>> {
+    /// # Errors
+    ///
+    /// This function will return an error if the texture cannot be created from the buffer object
+    ///
+    pub fn set_texture(&mut self, tid: u64, bo: &gbm::BufferObject<()>) -> Result<(), anyhow::Error> {
         let texture = Texture::new_from_dmabuf(
             self.env.get_display(),
-            bo.fd_for_plane(0)?,
-            bo.width()?,
-            bo.height()?,
-            bo.format()? as u32,
-            bo.offset(0)?,
-            bo.stride_for_plane(0)?,
-            bo.modifier()?.into()
-        )?;
+            bo.fd_for_plane(0).unwrap(),
+            bo.width().unwrap(),
+            bo.height().unwrap(),
+            bo.format().unwrap() as u32,
+            bo.offset(0).unwrap(),
+            bo.stride_for_plane(0).unwrap(),
+            bo.modifier().unwrap().into()
+        ).map_err(|e| anyhow!(e))?;
+        debug!("created new texture from dmabuf: tid={}, bo={:?}", tid, bo);
 
         self.textures.insert(tid, texture);
         Ok(())
@@ -104,9 +117,15 @@ impl RenderPipeline {
     /// * `vert` - The vertex shader path
     /// * `frag` - The fragment shader path
     ///
-    pub fn set_shader(&mut self, sid: u64, tids: &[u64], width: u32, height: u32, vert: &PathBuf, frag: &PathBuf) -> Result<(), Box<dyn Error>> {
-        let shader_program = Shader::new(vert, frag, tids)?;
+    /// # Errors
+    ///
+    /// This function will return an error if the shader program cannot be created
+    ///
+    pub fn set_shader(&mut self, sid: u64, tids: &[u64], width: u32, height: u32, vert: &PathBuf, frag: &PathBuf) -> Result<(), anyhow::Error> {
+        let shader_program = Shader::new(vert, frag, tids).context("failed to create shader program")?;
         let framebuffer = Framebuffer::new(width, height);
+        debug!("created new shader program: sid={}, framebuffer={}", sid, framebuffer.id);
+
         self.shader_program.insert(sid, (shader_program, framebuffer));
         Ok(())
     }
@@ -135,10 +154,12 @@ impl RenderPipeline {
             gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, std::ptr::null());
             gl::Flush();
         }
+        trace!("render pipeline rendered: sid={}, framebuffer={}", sid, framebuffer.id);
 
         shader.unbind(&textures);
 
         unsafe { gl::ReadPixels(0, 0, framebuffer.width as i32, framebuffer.height as i32, gl::RGB, gl::UNSIGNED_BYTE, pixels.as_mut_ptr() as *mut std::ffi::c_void); }
+        trace!("read pixels from framebuffer: sid={}, framebuffer={}", sid, framebuffer.id);
 
         framebuffer.unbind();
     }
@@ -147,10 +168,10 @@ impl RenderPipeline {
 
 impl Drop for RenderPipeline {
     fn drop(&mut self) {
-        unsafe { // why not, lol
-            gl::Disable(gl::TEXTURE_2D);
-
-            self.vertex_array.unbind();
-        }
+        debug!("dropping render pipeline, this will destroy all gl objects as well as the egl environment");
+        self.vertex_array.as_ref().unwrap().unbind();
+        self.textures.clear();
+        self.shader_program.clear();
+        self.vertex_array = None;
     }
 }
